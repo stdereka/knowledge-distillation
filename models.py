c66dd4a7643e8a8bb529260c0fa431db6abce099
stdereka <st.dereka@gmail.com>
2020-09-19 22:11:19 +0300

    Resnet152 teacher model

--- models.py
+++ models.py
@@ -64,6 +64,30 @@ def resnet18_student1(n_classes: int, device: str):
     return resnet
 
 
+def resnet152_teacher(n_classes: int, device: str):
+    resnet = torchvision.models.resnet152(pretrained=True)
+
+    for param in resnet.parameters():
+        param.requires_grad = False
+
+    in_features = resnet.fc.in_features
+
+    header = nn.Sequential(
+        nn.Dropout(0.975),
+        nn.Linear(in_features, 2048),
+        nn.ReLU(),
+        nn.Dropout(0.975),
+        nn.Linear(2048, 2048),
+        nn.ReLU(),
+        nn.Linear(2048, n_classes)
+    )
+
+    resnet.fc = header
+    resnet = resnet.to(device)
+
+    return resnet
+
+
 def get_number_of_params(model, trainable=True):
     if trainable:
         model_parameters = filter(lambda p: p.requires_grad, model.parameters())
