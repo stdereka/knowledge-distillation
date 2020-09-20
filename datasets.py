ffb733014a7c1b8832aef588349165a4a90b05d0
stdereka <st.dereka@gmail.com>
2020-09-20 18:56:41 +0300

    Imagewoof augs

--- datasets.py
+++ datasets.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class Imagewoof(Dataset):
-    def __init__(self, files: list, label_encoder: LabelEncoder, teacher_labels=None, size=256):
+    def __init__(self, files: list, label_encoder: LabelEncoder, teacher_labels=None, size=256, augs=False):
         self.files = files
 
         self.labels = [path.parent.name for path in self.files]
@@ -22,12 +22,27 @@ class Imagewoof(Dataset):
                                                    transforms.ToTensor(),
                                                    ])
 
+        if augs:
+            self.augs = transforms.Compose([
+                transforms.RandomApply([
+                    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+                    transforms.RandomAffine(degrees=(-30, 30), scale=(0.75, 1.25))
+                ], p=0.7),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomPerspective(p=0.5)
+            ])
+        else:
+            self.augs = None
+
     def __getitem__(self, index):
         path = self.files[index]
         label = self.labels[index]
 
         image = Image.open(path).convert('RGB')
         image.load()
+
+        if self.augs:
+            image = self.augs(image)
 
         image = self.transformations(image)
 
